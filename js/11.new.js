25d4294435d7cd50e05b3ac9b1540ec87b3c0133
xiejiaxin <>
2021-10-18 18:30:52 +0800

    commit

--- js/11.new.js
+++ js/11.new.js
@@ -2,6 +2,17 @@
  * @author: xiejiaxin
  * @Date: 2021-10-16 17:41:23
  * @LastEditors: xiejiaxin
- * @LastEditTime: 2021-10-16 17:41:23
+ * @LastEditTime: 2021-10-18 18:30:03
  * @description: file content
  */
+// new操作，实际上就是给实例对象赋值为原型对象的prototype
+
+let myNew = function(fn) {
+    
+}
+
+function A() {
+    
+}
+
+let a = myNew A();
