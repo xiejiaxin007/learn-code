4c8e40b609ad8185bbf668d178bf076e0623ca66
GRANDSOFT\xiejx <xiejx@glodon.com>
2021-12-08 16:23:03 +0800

    翻转二叉树

--- leetcode/8.翻转二叉树.js
+++ leetcode/8.翻转二叉树.js
@@ -1,63 +1,63 @@
 /*
  * @Author: xiejiaxin
  * @Date: 2021-11-17 14:19:02
- * @LastEditors: xiejiaxin
- * @LastEditTime: 2021-11-17 16:19:22
- * @Description: 
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-08 16:22:34
+ * @Description:
  * @FilePath: \learn-code\leetcode\8.翻转二叉树.js
  */
 // 翻转一棵二叉树。
 
 function TreeNode(val, left, right) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
 }
 
 const node = {
-    val: 4,
+  val: 4,
+  left: {
+    val: 2,
     left: {
-        val: 2,
-        left: {
-            val: 1,
-            left: null,
-            right: null
-        },
-        right: {
-            val: 3,
-            left: null,
-            right: null
-        }
+      val: 1,
+      left: null,
+      right: null,
     },
     right: {
-        val: 7,
-        left: {
-            val: 6,
-            left: null,
-            right: null
-        },
-        right: {
-            val: 9,
-            left: null,
-            right: null
-        }
-    }
-}
-
+      val: 3,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 7,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 9,
+      left: null,
+      right: null,
+    },
+  },
+};
 
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
+//! 自己写的
 var invertTree = function (root) {
-    if (root === null) {
-        return null;
-    }
-    let temp = root.left;
-    root.left = root.right;
-    root.right = temp;
-    invertTree(root.left);
-    invertTree(root.right);
-    return root;
+  if (!root && root === null) {
+    return null;
+  }
+  let left = root.left;
+  root.left = root.right;
+  root.right = left;
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
 };
 console.log(invertTree(node));
