1144a57644ce706745e5cbe5f917583f4aaa2bdc
GRANDSOFT\xiejx <xiejx@glodon.com>
2022-01-22 17:26:43 +0800

    二叉树填补

--- leetcode/12.填充二叉树.js
+++ leetcode/12.填充二叉树.js
@@ -1,8 +1,8 @@
 /*
  * @Author: xiejiaxin
  * @Date: 2021-11-17 16:19:11
- * @LastEditors: xiejiaxin
- * @LastEditTime: 2021-11-17 16:39:36
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-22 17:04:40
  * @Description: https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
  * @FilePath: \learn-code\leetcode\12.填充二叉树.js
  */
@@ -16,21 +16,116 @@
 // 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
 // 初始状态下，所有 next 指针都被设置为 NULL。
 
-var connect = function (root) {
-    if (root === null) {
-        return null;
-    }
-    connectTwoNode(root.left, root.right);
-    return root;
+// const node = {
+//   val: 0,
+//   left: null,
+//   right: null
+// }
+const node = {
+  val: -1,
+  left: {
+    val: 0,
+    left: {
+      val: 2,
+      left: {
+        val: 6,
+        left: null,
+        right: null
+      },
+      right: {
+        val: 7,
+        left: null,
+        right: null
+      },
+    },
+    right: {
+      val: 3,
+      left: {
+        val: 8,
+        left: null,
+        right: null
+      },
+      right: {
+        val: 9,
+        left: null,
+        right: null
+      },
+    },
+  },
+  right: {
+    val: 1,
+    left: {
+      val: 4,
+      left: {
+        val: 10,
+        left: null,
+        right: null
+      },
+      right: {
+        val: 11,
+        left: null,
+        right: null
+      },
+    },
+    right: {
+      val: 5,
+      left: {
+        val: 12,
+        left: null,
+        right: null
+      },
+      right: {
+        val: 13,
+        left: null,
+        right: null
+      },
+    },
+  },
 };
 
-var connectTwoNode = function(node1, node2) {
-    if (node1 === null || node2 === null) {
-        return;
-    }
-    node1.next = node2;
-    connectTwoNode(node1.left, node1.right);
-    connectTwoNode(node2.left, node2.right);
-    connectTwoNode(node1.right, node2.left);
+var connect1 = function (root) {
+  if (root === null) {
+    return null;
+  }
+  if (root.left === null || root.right === null) {
+    root.next = null;
+    return root
+  }
+  root.next = null;
+  root.left.next = root.right;
+  root.right.next = null;
+  connectNode(root.left, root.right);
+  return root
+};
+// 两个节点合并后指定next，这样
+var connectNode1 = function (leftNode, rightNode) {
+  if (leftNode.left === null || rightNode.left === null) {
+    return
+  }
+  leftNode.next = rightNode;
+  leftNode.left.next = leftNode.right;
+  leftNode.right.next = rightNode.left;
+  rightNode.left.next = rightNode.right;
+  rightNode.right.next = null;
+  connectNode(leftNode.left, leftNode.right)
+  connectNode(rightNode.left, rightNode.right)
+};
+
+
+var connect = function (root) {
+  if (root === null) {
+    return null
+  }
+  connectNode(root.left, root.right)
+  return root
+}
+var connectNode = function (leftNode, rightNode) {
+  if (leftNode === null || rightNode === null) {
+    return null
+  }
+  leftNode.next = rightNode
+  connectNode(leftNode.left, leftNode.right)
+  connectNode(rightNode.left, rightNode.right)
+  connectNode(leftNode.right, rightNode.left)
 }
 console.log(connect(node));
--- leetcode/8.翻转二叉树.js
+++ leetcode/8.翻转二叉树.js
@@ -2,8 +2,8 @@
  * @Author: xiejiaxin
  * @Date: 2021-11-17 14:19:02
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-12-08 16:22:34
- * @Description:
+ * @LastEditTime: 2021-12-08 16:42:18
+ * @Description:https://leetcode-cn.com/problems/invert-binary-tree/
  * @FilePath: \learn-code\leetcode\8.翻转二叉树.js
  */
 // 翻转一棵二叉树。
