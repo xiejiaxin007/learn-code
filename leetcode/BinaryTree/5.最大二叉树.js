4d78d71df7b64d671f777acfe9988b4d170ed144
xiejiaxin007 <>
2022-02-13 17:54:49 +0800

    最大二叉树

---
+++ leetcode/BinaryTree/5.最大二叉树.js
@@ -0,0 +1,44 @@
+/*
+ * @author: xiejiaxin
+ * @Date: 2022-02-13 16:43:17
+ * @LastEditors: xiejiaxin
+ * @LastEditTime: 2022-02-13 17:54:33
+ * @description: https://leetcode-cn.com/problems/maximum-binary-tree/
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+const params = [3,2,1,6,0,5];
+/** 看起来是个中序遍历
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+let node = {}
+var constructMaximumBinaryTree = function(nums) {
+    if (nums.length === 0) {
+        return;
+    }
+    if (nums.length === 1) {
+        return new TreeNode(nums[0])
+    }
+    const max = Math.max(...nums);
+    let root = new TreeNode(max);
+    const index = nums.findIndex(i => i === max);
+    const leftArr = nums.slice(0, index);
+    const rightArr = nums.slice(index + 1);
+    root.left = constructMaximumBinaryTree(leftArr)
+    root.right = constructMaximumBinaryTree(rightArr);
+    return root
+};
+// 中序遍历模板
+function forFn(root) {
+    if (root === null) {
+        return
+    }
+    forFn(root.left)
+    console.log(root)
+    forFn(root.right)
+}
+console.log(constructMaximumBinaryTree(params))
